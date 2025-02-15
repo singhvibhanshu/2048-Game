0eab49355751b4b9b66a669a46b7ea65c7ee796c
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-15 15:59:35 +0530

    constants definitions added

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import pygame, random, math
+
+pygame.init()
+
+FPS = 60
+
+HEIGHT, WIDTH = 800, 800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+OUTLINE_COLOUR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOUR = (205, 192, 180)
+FONT_COLOUR = (119, 110, 101)
+
+WINDOW = pygame.display.set_mode((HEIGHT, WIDTH))
+pygame.display.set_caption("2048")
+
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+MOVE_VEL = 20
