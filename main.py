5c0c4a11aae230f78c80d5e9d301bb2904db04b5
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-15 23:59:07 +0530

    moving tiles implementation pt 1

--- main.py
+++ main.py
@@ -61,10 +61,16 @@ class Tile:
         )
         
     def move(self, delta):
-        pass
+        self.x += delta[0]
+        self.y += delta[1]
 
-    def set_pos(self):
-        pass
+    def set_pos(self, ceil = False):
+        if ceil:
+            self.row = math.ceil(self.y / RECT_HEIGHT)
+            self.col = math.ceil(self.x / RECT_WIDTH)
+        else:
+            self.row = math.floor(self.y / RECT_HEIGHT)
+            self.col = math.floor(self.x / RECT_WIDTH)
 
 
 
@@ -101,6 +107,49 @@ def get_random_pos(tiles):
             break
 
     return row, col
+
+def move_tiles(window, tiles, clock, direction):
+    updated = True
+    blocks = set()
+
+    if direction == "left":
+        sort_func = lambda x: x.col
+        reverse = False
+        delta = (-MOVE_VEL, 0)
+        boundary_check = lambda tile: tile.col == 0
+        get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col - 1}")
+        merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_VEL
+        move_check = (
+            lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
+        )
+        ceil = True
+
+    while updated:
+        clock.tick(FPS)
+        updated = False
+        sorted_tiles = sorted(tiles.values(), key=sort_func, reverse=reverse)
+
+        for i, tile in enumerate(sorted_tiles):
+            if boundary_check(tile):
+                continue
+
+            next_tile = get_next_tile(tile)
+            if not next_tile:
+                tile.move(delta)
+            elif (tile.value == next_tile.value and tile not in blocks and next_tile not in blocks):
+                if merge_check(tile, next_tile):
+                    tile.move(delta)
+                else:
+                    next_tile.value *= 2
+                    sorted_tiles.pop(i)
+                    blocks.add(next_tile)
+            elif move_check(tile, next_tile):
+                tile.move(delta)
+            else:
+                continue
+
+            tile.set_pos(ceil)
+            updated = True
 
 def generate_tiles():
     tiles = {}
